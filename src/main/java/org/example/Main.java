986208183d0269cbda81494f0655585414213273
Матвеев Евгений Евгеньевич (4065045) <eematveev@vtb.ru>
2024-03-30 16:26:12 +0300

    1 point ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Start!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String Str = new AnnotationConfigApplicationContext("org.example")
+                .getBean("StringBean",SomeStringClass.class)
+                .make();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Str - " + Str);
+
+        System.out.println("Finish!");
+
+
     }
 }
---
+++ src/main/java/org/example/SomeStringClass.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SomeStringClass {
+    @Autowired String F1;
+
+    public SomeStringClass(String f1) {
+        F1 = "Hellow world";
+    }
+
+    public SomeStringClass() {
+        F1 = "Hellow world";
+    }
+
+
+    @Bean(name = "StringBean")
+    public String make() {
+        return F1;
+    }
+}
