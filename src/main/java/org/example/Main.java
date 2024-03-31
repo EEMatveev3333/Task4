6c823b1ae6b6a861124de3e6ff1473ccdf9aa5f5
Матвеев Евгений Евгеньевич (4065045) <eematveev@vtb.ru>
2024-03-31 12:16:36 +0300

    1 point ready temp2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,19 +12,27 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Start!");
 
+        // 1 task
         String Str = new AnnotationConfigApplicationContext("org.example")
                 .getBean("StringBean",SomeStringClass.class)
                 .make();
         System.out.println("Str - " + Str);
 
+        // 2 task
         Date Date_ = new java.util.Date();
 
         //new AnnotationConfigApplicationContext("org.example")
                 //.getBean("SomeDateClassBean1",SomeDateClass.class)
                 //.make();
-
-
         System.out.println("Date_ - " + Date_);
+
+        // 3 task
+        Integer Integer_;//_ = new java.util.Date();
+
+        Integer_ = new AnnotationConfigApplicationContext("org.example")
+        .getBean("SomePredicateInteger1",SomePredicateInteger.class)
+        .make();
+        System.out.println("Integer_ - " + Integer_);
 
         System.out.println("Finish!");
 
--- src/main/java/org/example/SomePredicateInteger.java
+++ src/main/java/org/example/SomePredicateInteger.java
@@ -1,21 +1,25 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
-//@Component
+@Component
 public class SomePredicateInteger {
 
 
-    Integer Fi;
+    @Autowired
+    private Integer Fi;
 
     public SomePredicateInteger(Integer fi) {
         Fi = fi;
     }
+    public SomePredicateInteger() {
+        Fi = 0;
+    }
 
     @Bean(name = "SomePredicateInteger1")
-    public Date make() {
+    public Integer make() {
         return Fi;
     }
 }
