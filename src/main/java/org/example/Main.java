89966f71d034fa20bbdb1dced6ba4d93f0eb4249
EEMatveev <eematveev333@gmail.com>
2024-03-31 15:44:05 +0300

    2 point ready temp4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,14 +9,14 @@ import java.util.Date;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.InterruptedException {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Start!");
 
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("org.example");
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // 1 task
         String Str = context
                 .getBean("StringBean",SomeStringClass.class)
@@ -27,14 +27,21 @@ public class Main {
                 .getBean("StringBean2",SomeStringClass2.class)
                 .make2();
         System.out.println("Str2 - " + Str2);
-        // 2 task
-        Date Date_ = new java.util.Date();
+
 
         //new AnnotationConfigApplicationContext("org.example")
         //Date_ =
         SomeStringClass2 someStringClass2  = context.getBean("SomeStringClass2",SomeStringClass2.class);
                 //.make();
         System.out.println("someStringClass2 - " + someStringClass2.make2());
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // 2 task
+        Date Date_ = new java.util.Date();
+        System.out.println("Date_ - " + Date_);
+        Thread.sleep(1000);
+        Date_ = context.getBean("SomeDateClassBean",SomeDateClass.class).getF1();
+        System.out.println("Date_ - " + Date_);
 
         // 3 task
         Integer Integer_;//_ = new java.util.Date();
--- src/main/java/org/example/SomeDateClass.java
+++ src/main/java/org/example/SomeDateClass.java
@@ -6,21 +6,34 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
+@Component("SomeDateClassBean")
 public class SomeDateClass {
-    @Autowired private Date F1;
+
+
+    //@Autowired
+    private Date F1;
+
+    public Date getF1() {
+        if (F1 == null) F1 = new java.util.Date();
+        return F1;
+    }
+
+    public void setF1(Date f1) {
+        if (F1 == null) f1 = new java.util.Date();
+    }
 
     public SomeDateClass(Date f1) {
-        F1 =  new java.util.Date();//"Hellow world";
+        F1 = null;//"Hellow world";
     }
 
     public SomeDateClass() {
-        F1 =  new java.util.Date();//"Hellow world";
+        F1 = null;//"Hellow world";
     }
 
 
-    @Bean(name = "SomeDateClassBean1")
-    public Date make() {
-        return F1;
-    }
+
+//    @Bean(name = "SomeDateClassBean1")
+//    public Date make() {
+//        return F1;
+//    }
 }
